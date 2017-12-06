90bbbc4e56fe85269284e94827128cf8892ddbd3
Martin X1 <martreg@posteo.de>
2017-12-06 23:56:12 +0100

    soft thresholding + WT improved

--- picIHT.py
+++ picIHT.py
@@ -2,6 +2,7 @@ from __future__ import print_function
 from __future__ import division
 import numpy as np
 import numpy.linalg as la
+import numbers
 np.set_printoptions(precision=3)
 import matplotlib.pyplot as plt
 import scipy.fftpack as spfft
@@ -51,7 +52,7 @@ class DCT(AbstractOperator):
         self.shape = shape
     
     def __call__(self, image):
-        Timage = spfft.dct(spfft.dct(image, norm='ortho', axis=0), norm='ortho', axis=1)
+        Timage = spfft.dct(spfft.dct(image, norm='ortho', axis=0, overwrite_x=True), norm='ortho', axis=1, overwrite_x=True)
         return Timage.reshape(-1)
     
     def inv(self, Timage):
@@ -60,12 +61,19 @@ class DCT(AbstractOperator):
     
 class WT(AbstractOperator):
     '''wavelet transform... under construction'''
-    def __init__(self, shape, wavelet = 'db6', level = 5, amplify = [1]):
+    def __init__(self, shape, wavelet = 'db6', level = 5, amplify = None):
         self.shape = shape
         self.wavelet = wavelet
         self.level = level
         self.cMat_shapes = [] 
-        self.amplify = amplify + (level-len(amplify)+1)*[1]
+        #build amplification vector of length 3*level
+        if amplify is None:
+            self.amplify = np.ones(3*self.level+1)
+        else:
+            self.amplify = amplify
+        if isinstance(amplify, numbers.Number):
+            self.amplify = np.ones(3*self.level+1)
+            self.amplify[0] = amplify       
     
     def __call__(self, image):
         coeffs = pywt.wavedec2(image, wavelet=self.wavelet, level=self.level)
@@ -74,18 +82,21 @@ class WT(AbstractOperator):
         #to list of np.arrays
         #multiply with self.amplify[0] to have them more strongly weighted in compressions
         #tbd: implement others
-        cMat_list = [self.amplify[0]*coeffs[0]]
+        cMat_list = [coeffs[0]]
         for c in coeffs[1:]:
             cMat_list = cMat_list + list(c)
         #memorize all shapes for inv
-        self.cMat_shapes = map(np.shape,cMat_list)
+        self.cMat_shapes = list(map(np.shape,cMat_list))
         
         #array vectorization
         vect = lambda array: np.array(array).reshape(-1)
         
         #store coeffcient matrices as vectors in list
-        cVec_list = map(vect,cMat_list)
-
+        #cVec_list = map(vect,cMat_list)
+        
+        #apply amplification
+        cVec_list = [vect(cMat_list[j])*self.amplify[j] for j in range(3*self.level+1)]
+            
         return np.concatenate(cVec_list)
     
     def inv(self,wavelet_vector):
@@ -97,18 +108,21 @@ class WT(AbstractOperator):
             print("Call WT first to obtain shapes of coefficient matrices")
             return None
         
-        cVec_shapes = map(np.prod,self.cMat_shapes)
+        cVec_shapes = list(map(np.prod,self.cMat_shapes))
         
         split_indices = list(accumulate(cVec_shapes))
         
         cVec_list = np.split(wavelet_vector,split_indices)
+        
+        #reverse amplification
+        cVec_list = [cVec_list[j]/self.amplify[j] for j in range(3*self.level+1)]
 
         #back to level format
-        coeffs=[ np.reshape(cVec_list[0]/self.amplify[0],self.cMat_shapes[0]) ]
-        for j in xrange(self.level):
+        coeffs=[ np.reshape(cVec_list[0],self.cMat_shapes[0]) ]
+        for j in range(self.level):
             triple = cVec_list[3*j+1:3*(j+1)+1]
             triple = [np.reshape( triple[i], self.cMat_shapes[1 +3*j +i] ) 
-                     for i in xrange(3) ]
+                     for i in range(3) ]
             coeffs = coeffs + [tuple(triple)]
 
         return pywt.waverec2( coeffs, wavelet=self.wavelet )
@@ -119,19 +133,38 @@ def pltPic(X):
     plt.show()
 
 def cL(s,x):
-    '''returns n-s abs-smallest inices of vector x'''
+    '''returns n-s abs-smallest indices of vector x'''
     ns = len(x)-s
     return np.argpartition(abs(x),ns)[:ns]    
 
-def thresholdingOperator(s,x):
-    '''takes vector x, returns hard thresholded vector'''
-    x[cL(s,x)] = 0
-    return x
-
-def compress(T,s,image):
-    '''returns compressed image by keeping the s largest coeffcients in dictionary T'''
+class hardTO(object):
+    '''Hard thresholding operator:
+            takes vector x, returns hard thresholded vector'''
+    def __init__(self,sparsity):
+        '''s: sparsity (integer number)'''
+        self.s = sparsity
+        
+    def __call__(self,x):
+        x[cL(self.s,x)] = 0
+        return x
+    
+class softTO(object):
+    '''Soft thresholding operator:
+        takes vector x, returns hard thresholded vector'''
+    def __init__(self,tau):
+        '''tau>0: thresholding parameter'''
+        self.tau = tau
+    def __call__(self,x):
+        return pywt.threshold(x, self.tau, mode='soft')
+    
+def compress(T, TO, image):
+    '''returns compressed image by appyling thresholding to coeffcients in dictionary T:
+    T: transformation taking image to vector, subclass of AbstractOperator
+    thresholding = (H,thresholding_parameter): 
+        H(v,thresholding_parameter) gives a vector for a vector v
+    image: matrix of black-white values'''
     x = T(image)
-    x = thresholdingOperator(s,x)
+    x = TO(x)
     Cimage = T.inv(x)
     # print error
     print("Relative error: {}".format( la.norm(Cimage-image,'fro')/la.norm(image,'fro') ))
@@ -141,9 +174,15 @@ def getRandMask(N,m):
     '''Random sample of m indices in range(N)'''
     return np.random.choice(N, m, replace=False)
 
-def update(T, s, mask, Xsub, X, mu):
-    '''IHT-type update, returns updated matrix Xnew and T-support of Xnew ''' 
-    Xm = np.zeros(X.shape)
+def update(T, thOp, mask, Xsub, X, mu):
+    '''IHT-type update, returns updated matrix Xnew and T-support of Xnew
+    T: transform
+    TO: thresholding operator
+    mask: indices with unknown pixels
+    Xsub: image matrix with Xsub[mask] arbitrary
+    mu: step size'''
+    
+    Xm = np.zeros(T.shape)
     Xm.flat[mask] = X.flat[mask]
     
     #calc gradient of squared L2-norm
@@ -152,20 +191,17 @@ def update(T, s, mask, Xsub, X, mu):
     
     #gradient step, transform
     TXnew = T( X-mu*grad )
-    
-    #partition according to s largest values
-    ns = len(TXnew)-s
-    part = np.argpartition(abs(TXnew),ns)
-    support = part[-s:]
-    cSupport = part[:ns]
-    
+          
     #threshold
-    TXnew[cSupport]=0
+    TXnew = thOp(TXnew)
     
-    Xnew = T.inv(TXnew)
-    return (Xnew, norm_grad, support)
+    #calculate support
+    support = TXnew==0
+    
+    return ( T.inv(TXnew), norm_grad, support )
 
-def estimate(T, s, mask, Xsub, stepsize = 1, n_steps = 50, X0=None):
+
+def estimate(T, thOp, mask, Xsub, stepsize = 1, n_steps = 100, X0=None):
     '''IHT-type estimate
     
     :param T: transfrom on pictures, e.g., DCT
@@ -181,22 +217,34 @@ def estimate(T, s, mask, Xsub, stepsize = 1, n_steps = 50, X0=None):
         print("Relative error (support change): {:3.2f}".format( la.norm(X-X0,'fro')/la.norm(X0,'fro') ), end = ', ')
     else:
         print("Support change: ")
-    for j in xrange(n_steps):
+        
+    for j in range(n_steps):
         #update
-        X, norm_grad, support = update(T, s, mask, Xsub, X, mu)
-        if j>=1:
-            support_diff = np.sum( support == last_support )
-            print(' ({})'.format(support_diff),end ='')
-        elif isinstance(X0,np.ndarray):
-            print('(-)', end='')
-        last_support = support
-        if isinstance(X0,np.ndarray):
-            rel_error = la.norm(X-X0,'fro')/la.norm(X0,'fro')
-            if rel_error>10: break
-            print(", {:3.2f}".format( rel_error ), end = '')            
-        #interrupt if diverging
-        elif la.norm(X,'fro')> norm0*np.prod(T.shape)/np.sqrt(s):
-            break            
+        X, norm_grad, support = update(T, thOp, mask, Xsub, X, mu)
+        #set negative values to zero
+        #X = pywt.threshold(X, 0, mode='greater', substitute = 0)
+        X = proj2range(X)
+        #print output
+        if j % 10 == 0:
+            if j>=1:
+                support_diff = np.sum( support == last_support )
+                print(' ({})'.format(support_diff),end ='')
+            elif isinstance(X0,np.ndarray):
+                print('(-)', end='')
+            last_support = support
+            if isinstance(X0,np.ndarray):
+                rel_error = la.norm(X-X0,'fro')/la.norm(X0,'fro')
+                if rel_error>10: break
+                print(", {:3.2f}".format( rel_error ), end = '')            
+            #interrupt if diverging
+            elif la.norm(X,'fro')> norm0*np.prod(T.shape)/np.sqrt(s):
+                break    
+    return X
+
+def proj2range(X):
+    '''Projects array elements to interval [0,255]'''
+    X = pywt.threshold(X, 255, mode='less', substitute = 255)
+    X = pywt.threshold(X, 0, mode='greater', substitute = 0)
     return X
 
 def rand_ux(N,s):
